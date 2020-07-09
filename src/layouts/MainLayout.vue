36a25fb51cf4cc2b214caabb25be970bc61f1bcd
Techdoodle <26680599+TheTechdoodle@users.noreply.github.com>
2020-07-08 19:42:04 -0700

    add quasar

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,22 @@
+<template>
+    <q-layout view="hHh lpr fff">
+        <q-header elevated>
+            <q-toolbar>
+                <q-toolbar-title>
+                    Netflix Party Web
+                </q-toolbar-title>
+            </q-toolbar>
+        </q-header>
+
+        <q-page-container>
+            <router-view/>
+        </q-page-container>
+    </q-layout>
+</template>
+
+<script>
+    export default {
+        name: 'MainLayout',
+        components: {}
+    };
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,12 @@
+<template>
+    <q-page class="flex flex-center column">
+        <h2>Hello</h2>
+        <h2>World</h2>
+    </q-page>
+</template>
+
+<script>
+    export default {
+        name: 'PageIndex'
+    };
+</script>
